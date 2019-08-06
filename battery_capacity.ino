d17172ed5ad2327cfe73257527e3269f33f0389e
Ricky-hihi <53637778+Ricky-hihi@users.noreply.github.com>
2019-08-06 13:14:13 +0800

    Add files via upload

--- battery_capacity.ino
+++ battery_capacity.ino
@@ -1,4 +1,4 @@
-int analogPin = A3 ;
+int analogPin = A1 ;
 int MyArray[11] = {0} ;
 int val ;
 byte blank ;
@@ -9,22 +9,14 @@ void setup() {
 }
 
 void loop() {
+  
   val = map(analogRead(analogPin),720,850,0,100) ;
-//
-  Serial.println("排序前 : ") ;
-//  
   // 把感測器值填入陣列中
   for (byte i = 0 ; i<11 ; i++){
     MyArray[i] = {val} ;
     delay(100) ;
     }
-//
-  // 印出目前未排序的陣列
-  for (byte j = 0 ; j<11 ; j++){
-    Serial.println(MyArray[j]) ;
-    }
-  Serial.println("排序後 : ") ;
-//
+    
   // 把陣列中的值由大排到小
     for (byte m = 0 ; m<11 ; m++ ){
       for (byte n = m+1 ; n<11 ; n++){
@@ -35,13 +27,8 @@ void loop() {
           }
         } 
       }
- 
-    // 印出排序後的陣列
-//
-    for (byte p = 0 ; p<11 ; p++){
-      Serial.println(MyArray[p]) ;
-      }
-//
+      
+  // 印出排序後的陣列
     if (MyArray[5]<0 or MyArray[5]>100){
       if (MyArray[5]<0){
         Serial.print("battery capacity : ") ;Serial.print(0) ;Serial.println("%") ;
